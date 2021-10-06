ce1d3692893b82889438e87be3064fd82be2fad5
Paul Kovack <paulkovack@gmai.com>
2021-10-05 20:56:26 -0400

    Initial Spring Project Setup
    Files generated using start.spring.io to create a Spring Java 11 jar using Gradle. Included Spring Web, Spring JPA, and H2 Database dependencies.

---
+++ src/main/java/com/paulkovack/pk1021/Pk1021Application.java
@@ -0,0 +1,13 @@
+package com.paulkovack.pk1021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pk1021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pk1021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulkovack/pk1021/Pk1021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulkovack.pk1021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pk1021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
